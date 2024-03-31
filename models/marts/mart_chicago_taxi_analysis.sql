d40d2342c44117aabcbb52f3fdb69bcb1757e982
Anna Pasko <annapasko9889@gmail.com>
2024-03-31 21:17:48 +0000

    .

--- models/marts/mart_chicago_taxi_analysis.sql
+++ models/marts/mart_chicago_taxi_analysis.sql
@@ -4,9 +4,9 @@ SELECT
     td.month,
     td.tips_sum,
     (td.tips_sum - 
-    LAG(td.tips_sum, 1, 0) OVER (PARTITION BY td.taxi_id ORDER BY td.year, td.month)) /
-    NULLIF(LAG(td.tips_sum, 1, 0) OVER (PARTITION BY td.taxi_id ORDER BY td.year, td.month), 0) AS tips_change
+    LAG(td.tips_sum, 1, 0) OVER (partition by taxi_id ORDER BY td.year, td.month)) /
+    NULLIF(LAG(td.tips_sum, 1, 0) OVER (partition by taxi_id ORDER BY td.year, td.month), 0) AS tips_change
 FROM 
-    {{ ref ('stg_tips_data') }} as td
+    {{ ref ('stg_top_drivers') }} as td
 ORDER BY 
     td.tips_sum DESC
